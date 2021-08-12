d0e9ecb03149c07fb3a1bdefb6bfb54d6ed329c7
Dang Tien Dat <48046754+Shawcicdd@users.noreply.github.com>
2021-08-13 03:18:02 +0700

    Jaccard metric

--- utils/cluster_utils.py
+++ utils/cluster_utils.py
@@ -13,6 +13,40 @@ def kmeans_clustering(embb_vec: np.ndarray,
     sum_squared = kmeans.inertia_
     return clusters, sum_squared
 
+def Jaccard_score(clusters,all_labels):
+    nber_real_class = len(set(all_labels))
+    nber_clusters = len(set(clusters))
+    dct_class={}
+    for i in list(set(all_labels)):
+        temp=[j for j,label in enumerate(all_labels) if label == i]
+        dct_class[i] = temp
+    dct_cluster={}
+    for i in list(set(clusters)):
+        temp=[j for j,label in enumerate(clusters) if label == i]
+        dct_cluster[i] = temp
+    matrix_Jaccard=np.zeros((nber_real_class,nber_clusters))
+    for i in range(len(list(set(all_labels)))):
+        for j in range(len(list(set(clusters)))):
+            matrix_Jaccard[i,j] = Jaccard_index_formular(dct_class[list(set(all_labels))[i]],
+            dct_cluster[list(set(clusters))[j]])
+    cluster_closest_labels = [list(set(clusters))[j] for j in list(np.argmax(matrix_Jaccard,axis = 1))]
+    label_closest_cluster =  [list(set(all_labels))[j] for j in list(np.argmax(matrix_Jaccard,axis = 0))]
+    print("thu tu label:",list(set(all_labels)))
+    print("cac label gan voi cluster theo thu tu:",cluster_closest_labels)
+    print("JC index tuong ung:",list(np.max(matrix_Jaccard,axis = 1)))
+    print("-----000------")
+    print("thu tu cluster:",list(set(clusters)))
+    print("cac cluster gan voi cac label theo thu tu:",label_closest_cluster)
+    print("JC index tuong ung:",list(np.max(matrix_Jaccard,axis = 0)))
+    print("*********")
+    print("matrix Jaccard:",matrix_Jaccard)
+    return 1
+def Jaccard_index_formular(set1,set2):
+    a=set(set1)
+    b=set(set2)
+    T= len(a & b)
+    M= len(a | b)
+    return T/M 
 def get_cluster_class(all_labels: np.ndarray,
                         clusters: np.ndarray) -> np.ndarray:
     """
